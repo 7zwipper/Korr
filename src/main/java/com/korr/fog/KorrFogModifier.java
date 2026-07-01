d5ffe49b8e285eacefcfb7f0f7a9c37b4eaf981c
7zwipper <vlav6014@gmail.com>
2026-07-01 12:53:23 +0500

    Create KorrFogModifier.java

---
+++ src/main/java/com/korr/fog/KorrFogModifier.java
@@ -0,0 +1,29 @@
+package com.korr.fog;
+
+import net.minecraft.block.enums.CameraSubmersionType;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.client.render.fog.FogData;
+import net.minecraft.client.render.fog.FogModifier;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+
+public class KorrFogModifier extends FogModifier {
+
+    // ~2-4 чанка (1 чанк = 16 блоков)
+    private static final float FOG_START = 32.0f;
+    private static final float FOG_END = 64.0f;
+
+    @Override
+    public void applyStartEndModifier(FogData data, Camera camera, ClientWorld clientWorld, float tickDelta, RenderTickCounter renderTickCounter) {
+        data.renderDistanceStart = FOG_START;
+        data.renderDistanceEnd = FOG_END;
+        data.environmentalStart = FOG_START;
+        data.environmentalEnd = FOG_END;
+    }
+
+    @Override
+    public boolean shouldApply(CameraSubmersionType submersionType, Entity cameraEntity) {
+        return submersionType == null || submersionType == CameraSubmersionType.NONE;
+    }
+}
