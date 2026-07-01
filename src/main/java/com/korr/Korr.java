a3e52edcdc4d116ae080ece71537845c82e3d22b
7zwipper <vlav6014@gmail.com>
2026-07-01 12:02:28 +0500

    Create Korr.java

---
+++ src/main/java/com/korr/Korr.java
@@ -0,0 +1,15 @@
+package com.korr;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Korr implements ModInitializer {
+    public static final String MOD_ID = "korr";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("[Korr] Initialized. Something is watching.");
+    }
+}
